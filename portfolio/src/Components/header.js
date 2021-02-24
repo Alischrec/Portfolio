449d223ec8cd10d76635867e8d374808dc16cb8d
Alischrec <alischreck@gmail.com>
2021-02-23 18:30:35 -0800

    added TypeIt text and styling

--- portfolio/src/Components/header.js
+++ portfolio/src/Components/header.js
@@ -1,16 +1,20 @@
 import react from 'react';
 import { Image } from 'react-bootstrap';
-// import TypeIt from "typeit";
+import TypeIt from "typeit-react";
 
 function Header(props) {
-    // new TypeIt("#element", {
-    //     strings: "I write code with a focus on an easy to use UX.",
-    //   }).go();
     return (
         <section className="container">
             <Image src="/images/headshot.jpg" className="headshot" alt="headshot" />
-            <h1 className="title">Web Developer & Support Specialist</h1>
-            <p className="subtitle" id="element"></p>
+            <div className='center'>
+                <h1 className="title">Web Developer & Technical Support Specialist</h1>
+                <TypeIt className="subtitle"
+                    options={{
+                        strings: ["I write code with a focus on an easy to use UX."],
+                        speed: 50,
+                        waitUntilVisible: true
+                    }} />
+            </div>
             <Image src="/images/canva.png" className="hero img-fluid" alt="computer and other technologies" />
         </section>
     )
