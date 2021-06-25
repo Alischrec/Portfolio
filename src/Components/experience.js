59699ee9f9e48819a1d8e833539fcf7d0cd34982
Alischrec <alischreck@gmail.com>
2021-06-24 19:39:46 -0700

    added new experience and fixed bug

--- src/Components/experience.js
+++ src/Components/experience.js
@@ -2,7 +2,7 @@ import react from 'react';
 
 function Experience(props) {
     return (
-        <section class="container skills" >
+        <section className="container skills" >
             <div className="profile-card" data-speed="2">
                 <h5 className="card-title">Web Developer</h5>
                 <p className="card-text">
@@ -20,39 +20,63 @@ function Experience(props) {
                 </h5>
                 <p className="card-text">
                     Bootstrap<br />
-                        Materialize<br />
-                        Foundation<br />
-                        Github & Gitlab<br />
-                        Azure DevOps<br />
-                        VSCode<br />
+                    Materialize<br />
+                    Foundation<br />
+                    Github & Gitlab<br />
+                    Azure DevOps<br />
+                    VSCode<br />
                 </p>
 
             </div>
 
             <div className="profile-card">
-                <h5 className="card-title">Technical Support <br></br> Specialist</h5>
-                <p className="card-text">Providing technical support via online live chat while troubleshooting basic
-                        technological issues.</p>
+                <h5 className="card-title">Teaching Assistant</h5>
+                <p className="card-text">Mentor students who are part of the Full Stack Web Development Boot Camp with Trilogy Education.</p>
                 <h5 className="card-title">
-                    Technologies I use:
-                        </h5>
+                    Frameworks:
+                </h5>
                 <p className="card-text">
-                    Happy Fox, Looker, Zendesk, Intercom, Salesforce, Outreach, HubSpot, Notion, Quip, Azure DevOps,
-                    SendWithUs, Skilljar, Customer Success Box
-                        </p>
-                <h5 className="card-title">
-                    Notable Stats:
-                        </h5>
-                <p className="card-text">
-                    2021: 5/5 star raiting<br />
-                                2020: 4.9/5 star raiting<br />
-                                Average Response Time (ART):<br />
-                                2021: 0.14 seconds<br />
-                                2020: 0.17 seconds<br />
+                    React.js <br></br>
+                    Express.js <br></br>
+                    Node.js <br></br>
                 </p>
+                <h5 className="card-title">
+                    Languages:
+                </h5>
+                <p className="card-text">
+                    JavaScript (ES6 standards) <br></br>
+                    HTML <br></br>
+                    CSS<br></br>
+                    SQL(MySQL, Sequelize) <br></br>
+                </p >
 
-            </div>
-        </section>
+            </div >
+
+        <div className="profile-card">
+            <h5 className="card-title">Technical Support <br></br> Specialist</h5>
+            <p className="card-text">Providing technical support via online live chat while troubleshooting basic
+                technological issues.</p>
+            <h5 className="card-title">
+                Technologies I use:
+            </h5>
+            <p className="card-text">
+                Happy Fox, Looker, Zendesk, Intercom, Salesforce, Outreach, HubSpot, Notion, Quip, Azure DevOps,
+                SendWithUs, Skilljar, Customer Success Box
+            </p>
+            <h5 className="card-title">
+                Notable Stats:
+            </h5>
+            <p className="card-text">
+                2021: 5/5 star raiting<br />
+                2020: 4.9/5 star raiting<br />
+                Average Response Time (ART):<br />
+                2021: 0.14 seconds<br />
+                2020: 0.17 seconds<br />
+            </p>
+
+        </div>
+
+        </section >
     )
 }
 
