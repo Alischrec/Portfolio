8ab6913d8238716f892070c9771472eaf5a39d5a
Alischrec <alischreck@gmail.com>
2022-04-23 12:49:24 -0700

    footer date

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -3,7 +3,7 @@ import react from 'react';
 function Footer(props) {
     return (
         <footer>
-            Made by Ali-Schreck. Copyright &copy; | 2021
+            Made by Ali-Schreck. Copyright &copy; | 2022
         </footer>
     );
 }
